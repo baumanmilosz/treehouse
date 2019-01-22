957db24ff5e5160249a27201ed378f8eb80f236a
milosz.bauman <bauman.milosz@gmail.com>
2019-01-22 23:24:37 +0100

    Add all files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*HAMBURGER MENU*/
+const hamburger = document.querySelector('.hamburger');
+const toogleClass = () => {
+  document.querySelector('.page-header').classList.toggle('nav-opened');
+};
+hamburger.addEventListener('click', toogleClass);
+/*/HAMBURGER MENU*/
+
+/*SCROLL*/
+
+/*/SCROLL*/
